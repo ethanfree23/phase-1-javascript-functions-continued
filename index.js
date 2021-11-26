d9df4fc9d414895877750bd10c3f438e6ce5254e
Ethan Freeman <ethanfree23@gmail.com>
2021-11-26 16:23:16 -0600

    commit tree 3f1f095bc0db26415d6ddec15675466039e479a6

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
 }
 
-let mondayWork = function(activity="go to the office") {
+
+function mondayWork(activity='go to the office') {
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+function wrapAdjective(symbol = "*") {
+  return function (statement = "a hard worker") {
+    return `You are ${symbol}${statement}${symbol}!`;
   }
 }
